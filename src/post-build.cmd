a88a61e331407dfefbf897beca11e87ab1e5e9b3
antokhio <antokhio@gmail.com>
2024-10-17 19:59:07 +0300

    fix paths

--- src/post-build.cmd
+++ src/post-build.cmd
@@ -1,5 +1,5 @@
 echo %1 %2 %3 %4
 echo %1bin\%2\%3\%4*.dll
 
-xcopy %1bin\%2\%3\%4\*.dll %1..\..\lib\%2\%4 /h /i /c /k /e /r /y
+xcopy %1bin\%2\%3\%4\\*.dll %1..\..\lib\%2\%4 /h /i /c /k /e /r /y
 xcopy %1..\YogaSharp\yoga\build\Release\*.dll %1..\..\runtimes\win-x64\native /h /i /c /k /e /r /y
