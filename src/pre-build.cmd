03e871c9c0f987e037b3c1dee14812d622d523b7
antokhio <antokhio@gmail.com>
2024-10-18 15:28:19 +0300

    test .patch

--- src/pre-build.cmd
+++ src/pre-build.cmd
@@ -1,12 +1,14 @@
 IF [%1] == [] (
 	SET "_PATH=.\"
 ) ELSE (
-	SET "_PATH=%1"
+	SET "_PATH=%1..\"
 )
 
 echo %_PATH%
 
 git apply --ignore-whitespace %_PATH%..\.patch
+echo "appling patch %_PATH%..\.patch"
+
 
 cmake %_PATH% -B %_PATH%YogaSharp\yoga\build
 msbuild %_PATH%YogaSharp\yoga\build\yogacore.vcxproj /p:Configuration=Release /p:Platform=x64
